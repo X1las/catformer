57e9f4283822d7ccb12b56337eb117b0d96d8cb0
X1las <dahepix1@gmail.com>
2021-02-11 01:16:35 +0100

    making files and trying things out

--- jonescode/main.py
+++ jonescode/main.py
@@ -1,17 +1,17 @@
 # Initial:
 import pygame, sys                                      # Importing modules, pygame and sys so far             
+from settings import *                                  # Importing everything from settings
+from level import *                                     # Importing level data
+
 pygame.init()                                           # Initializing the pygame module
 
 # Screen:
-screen = pygame.display.set_mode((800,600))             # Creating a screen (possibly an object)
+screen = pygame.display.set_mode((WINDOW_W,WINDOW_H))   # Creating a screen (possibly an object)
 pygame.display.set_caption("Pygame Tutorial")           # Setting the screen caption
 
 # Variables:
-x = 50              
-y = 50
-height = 60
-width = 40
-vel = 5
+x = INIT_X              
+y = INIT_Y - C_HEIGHT
 
 # Runtime:
 running = True                                          # Creating a boolean for while loop
@@ -22,20 +22,22 @@ while running:                                          # While loop
     
     keys = pygame.key.get_pressed()                     # Assigning keys pressed in pygame module to variable
 
-    if keys[pygame.K_LEFT]:                             # checking if keys pressed contains left key and decrementing x by .1
+    # Movement:
+    if keys[pygame.K_LEFT]:                         
         x-=0.1
     
     if keys[pygame.K_RIGHT]:        
         x+=0.1
     
     if keys[pygame.K_DOWN]:
-        y+=0.1
+        y+=10
     
     if keys[pygame.K_UP]:
-        y-=0.1
+        y-=10
 
     screen.fill((100,100,100))                                              # Fills the entire screen with a gray color
-    pygame.draw.rect(screen, (255 , 0 , 0) , (x , y , width , height))      # Draws a rectangle on the screen
+    pygame.draw.rect(screen, (255 , 0 , 0) , (x , y , C_WIDTH , C_HEIGHT))  # Draws a rectangle on the screen
     pygame.display.update()                                                 # Updates the display?
 
-pygame.quit()                                                               # Terminates the process
+pygame.quit()                                                               # Terminates the pygame process
+sys.exit()                                                                  # Terminates the system process
---
+++ jonescode/settings.py
@@ -0,0 +1,16 @@
+# Character Size:
+C_HEIGHT = 60
+C_DUCKHEIGHT = 40
+C_WIDTH = 20
+
+# Movement:
+
+SPEED_MAX = 3
+SPEED_ACC = 0.3
+JUMP_FORCE = 6
+GRAVITY = 0.5
+
+# Screen:
+
+WINDOW_W = 1900
+WINDOW_H = 1080
