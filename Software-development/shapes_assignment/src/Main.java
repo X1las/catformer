d2eb2a6b393533073d4e474ae2abb719e9c1a773
KataRiegels <KataRiegels@users.noreply.github.com>
2021-03-02 10:49:15 +0100

    uck

--- Software-development/shapes_assignment/src/Main.java
+++ Software-development/shapes_assignment/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // sdfkjh
+        // sdfkjhlkajlkjlksjd
         int i = 0; int j = 0; int k = 0;
         String str = "poop"; int o = 9;
         System.out.println("fuck");
