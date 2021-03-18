e52f036c82b27355c4792663727c60797cef5461
KataRiegels <KataRiegels@users.noreply.github.com>
2021-03-18 10:04:27 +0100

    latest version

--- project/CustomSprite.py
+++ project/CustomSprite.py
@@ -5,122 +5,139 @@ vec = Vec
 
 # Classes
 class CustomSprite(pg.sprite.Sprite):
-    pos = vec()
-    vel = vec()
-    acc = vec()
-    height = None
-    width = None
+    pos = vec(); vel = vec(); acc = vec()
+    height = None; width = None
     game = None
+    groups = game.all_sprites
+
+
+
+    #attributes:
+    solid     = False
+    moveable  = False
+    breakable = False
+
+
+
     #groups = game.all_sprites
 
-    def rayIntersect(self,local_origin,col_objects):   
-        o = self.pos + local_origin     # Origin vector for calculations
-        v = self.vel                    # X and Y vector
-        col = col_objects               # Array of collideable objects
+    def rayIntersect(self,local_origin,collidables):   
+        original_pos = self.pos + local_origin     # Origin vector for calculations
+        vel = self.vel                    # X and Y vector
+        #collidables = col_objects               # Array of collideable objects
 
-        intersection = v.copy()         # Default intersection vector for comparison
+        intersection = vel.copy()         # Default intersection vector for comparison
         hitObject = False               # Hit object as false by default
 
         # Will check if the x and y vectors are not equal to 0 and assign a to their quotient if they are not
-        a = False                                               
-        if v.x != 0 and v.y != 0:                               
-            a = v.y/v.x                                         
+        slope = False                                               
+        if vel.x != 0 and vel.y != 0:                               
+            slope = vel.y/vel.x                                         
 
         # we use the linear function f(x) = ax+b
         # if a = y2-y1/x2-x1, then because we have origin in 0,0 x1 and y1 is 0
         # so long as v.x or v.y both aren't 0 then we can use the function as f(x) = a*x as b is 0
         # therefore y = a*x AND x = y/a
 
-        if a:
-            for c in col:
+        if slope:
+            for collidable in collidables:
                 # Vertical intersections:
-                y_temp_intersection = c.pos.y - c.height                                # y equals the tops if moving down
+                y_temp_intersection = collidable.pos.y - collidable.height                                # y equals the tops if moving down
                 
-                if v.y < 0:                                                             # If jumping
-                    y_temp_intersection = c.pos.y                                       # y equals the bottoms if moving up
+                if vel.y < 0:                                                             # If jumping
+                    y_temp_intersection = collidable.pos.y                                       # y equals the bottoms if moving up
 
-                y_local_temp = y_temp_intersection - o.y                                # Making a local y coordinate which is the y intersection - the origin's y position
-                x_local_temp = y_local_temp / a                                         # Making a local x coordinate from the local y divided by a
+                y_local_temp = y_temp_intersection - original_pos.y                                # Making a local y coordinate which is the y intersection - the origin's y position
+                x_local_temp = y_local_temp / slope                                         # Making a local x coordinate from the local y divided by a
                 
-                x_temp_intersection = o.x + x_local_temp                                # Using the local x and adding the origin's x to get global x coordinates of the intersection point
+                x_temp_intersection = original_pos.x + x_local_temp                                # Using the local x and adding the origin's x to get global x coordinates of the intersection point
                 
-                if c.pos.x - c.width/2 < x_temp_intersection < c.pos.x + c.width/2:     # Check if collision's x is between the collision object's left and right sides
+                if collidable.left_x() < x_temp_intersection < collidable.right_x():     # Check if collision's x is between the collision object's left and right sides
                     tempVec = vec(x_local_temp , y_local_temp)                          # Making a temporary vector be equal to the intersection
                     if tempVec.length() < intersection.length():                        # Checking if the temporary is shorter than the current intersection vector
                         intersection = tempVec                                          # If it's true, the intersection will be equal to the temporary
-                        hitObject = c                                                   # Hit object will be defined as c
+                        hitObject = collidable                                                   # Hit object will be defined as c
                 
                 # Horizontal intersections:
-                x_temp_intersection = c.pos.x - c.width/2                               # x equals left side if moving right
-                if v.x < 0:
-                    x_temp_intersection = c.pos.x + c.width/2                           # x equals right side if moving left
+                x_temp_intersection = collidable.left_x()                               # x equals left side if moving right
+                if vel.x < 0:
+                    x_temp_intersection = collidable.right_x()                           # x equals right side if moving left
                 
-                x_local_temp = x_temp_intersection - o.x                                # Making a local x coordinate which is the x intersection - origin's x position
-                y_local_temp = x_local_temp * a                                         # Making a local y coordinate from the local x multiplied by a        
+                x_local_temp = x_temp_intersection - original_pos.x                                # Making a local x coordinate which is the x intersection - origin's x position
+                y_local_temp = x_local_temp * slope                                         # Making a local y coordinate from the local x multiplied by a        
 
-                y_temp_intersection = o.y + y_local_temp                                # Global y position is the same as the origin's y + the local intersection's y position
+                y_temp_intersection = original_pos.y + y_local_temp                                # Global y position is the same as the origin's y + the local intersection's y position
                 
-                if c.pos.y - c.height < y_temp_intersection < c.pos.y:                  # check if collision's y is between the collision object's top and bottom sides
+                if collidable.top_y() < y_temp_intersection < collidable.bot_y():                  # check if collision's y is between the collision object's top and bottom sides
                     tempVec = vec(x_local_temp,y_local_temp)
                     if tempVec.length() < intersection.length():
                         intersection = tempVec
-                        hitObject = c 
+                        hitObject = collidable 
         # When we have cases where one side is 0
         # Reduce it later, when we can boil it down more
         # Refer to the bit above if confused, there's a lot of repitition
         else:
             # If v.x is not 0:
-            if v.x != 0:
-                for c in col:                                   
+            if vel.x != 0:
+                for collidable in collidables:                                   
                     # X intersection will be equal to the left side by default and the right side if we're moving left 
-                    x_temp_intersection = c.pos.x - c.width/2
-                    if v.x < 0:
-                        x_temp_intersection = c.pos.x + c.width/2
+                    x_temp_intersection = collidable.pos.x - collidable.width/2
+                    if vel.x < 0:
+                        x_temp_intersection = collidable.pos.x + collidable.width/2
                     
-                    x_local_temp = x_temp_intersection - o.x                               
+                    x_local_temp = x_temp_intersection - original_pos.x                               
                     y_local_temp = 0                                         
 
-                    y_temp_intersection = o.y + y_local_temp                                
+                    y_temp_intersection = original_pos.y + y_local_temp                                
                     
-                    if c.pos.y - c.height < y_temp_intersection < c.pos.y:                  
+                    if collidable.pos.y - collidable.height < y_temp_intersection < collidable.pos.y:                  
                         tempVec = vec(x_local_temp,y_local_temp)
                         if tempVec.length() < intersection.length():
                             intersection = tempVec
-                            hitObject = c 
+                            hitObject = collidable 
             # If v.y is above 0
-            if v.y != 0:
-                for c in col:
+            if vel.y != 0:
+                for collidable in collidables:
                     # Y intersection will be equal to the top side by default and the bottom if we're moving up
-                    y_temp_intersection = c.pos.y - c.height 
-                    if v.y < 0:                                                             
-                        y_temp_intersection = c.pos.y                                       
+                    y_temp_intersection = collidable.pos.y - collidable.height 
+                    if vel.y < 0:                                                             
+                        y_temp_intersection = collidable.pos.y                                       
 
-                    y_local_temp = y_temp_intersection - o.y                                
+                    y_local_temp = y_temp_intersection - original_pos.y                                
                     x_local_temp = 0                                        
                     
-                    x_temp_intersection = o.x + x_local_temp                                
+                    x_temp_intersection = original_pos.x + x_local_temp                                
                     
-                    if c.pos.x - c.width/2 < x_temp_intersection < c.pos.x + c.width/2:     
+                    if collidable.pos.x - collidable.width/2 < x_temp_intersection < collidable.pos.x + collidable.width/2:     
                         tempVec = vec(x_local_temp , y_local_temp)                          
                         if tempVec.length() < intersection.length():                        
                             intersection = tempVec                                          
-                            hitObject = c 
-            # Hi guys :-)
-            # Made with love and hate of algebra
+                            hitObject = collidable 
+ 
         
         if hitObject:
-            intersection += o # Adding the origin's vector in the end to return the global coordinates instead of the local
+            intersection += original_pos # Adding the origin's vector in the end to return the global coordinates instead of the local
             return [hitObject,intersection]
         else:
             return False
 
 
+    def top_y(self):
+        return self.pos.y - self.height
+    def bottom_y(self):
+        return self.pos.y
+    def left_x(self):
+        return self.pos.x - self.width/2
+    def right_x(self):
+        return self.pos.x + self.width/2
 
-    def loadImage():
+
+    def loadImage(self):
         pass
     
     def distributeAttributes(self, *attributes):
-        pass
+        for attribute in attributes:
+            attribute = True
     
     def distributeGroups(self, *groups):
         pass
