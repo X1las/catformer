06669c27b8a213f7628f1200a099af908c9eb858
KataRiegels <KataRiegels@users.noreply.github.com>
2021-05-10 12:05:40 +0200

    stine level activators functional

--- project/levelCreator.py
+++ project/levelCreator.py
@@ -76,8 +76,8 @@ def createLevel1():
             'length': 5000,
             'track': ''
         },
-        'platforms': [floor, fatplat, p_1, p_2, p_3, p_4, p_5],
-        'boxes':     [box1, box2],
+        'platforms': [floor, fatplat], # , p_1, p_2, p_3, p_4, p_5],
+        'boxes':     [], # box1, box2],
         'buttons':   [btn1, btn2],
         'levers':    [],
         'mugs':      [mug1],
